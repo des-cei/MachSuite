5f9864a2ad43d79573cc513ec13bca1e45a332ac
Alfonso Rodriguez <alfonso.rodriguezm@upm.es>
2020-02-03 20:04:14 +0100

    Added GEMM/BLOCKED (ARTICo³)

---
+++ tests/artico3/gemm/blocked/src/application/local_support.c
@@ -0,0 +1,135 @@
+#include "gemm.h"
+#include <string.h>
+#include <sys/time.h> // struct timeval, gettimeofday()
+#include "artico3.h"
+
+int INPUT_SIZE = sizeof(struct bench_args_t);
+
+#define EPSILON ((TYPE)1.0e-6)
+
+#define ITERATIONS (1024)
+#define ACCELERATORS (1)
+
+void run_benchmark( void *vargs ) {
+  struct bench_args_t *args = (struct bench_args_t *)vargs;
+  //~ bbgemm( args->m1, args->m2, args->prod );
+
+  struct timeval t0, tf;
+  float t;
+
+  artico3_init();
+
+  artico3_kernel_create("gemm", 49152, 3, 0);
+
+  gettimeofday(&t0, NULL);
+  for (int i = 0; i < ACCELERATORS; i++) {
+      artico3_load("gemm", i, 0, 0, 0);
+  }
+  gettimeofday(&tf, NULL);
+  t = ((tf.tv_sec - t0.tv_sec) * 1000.0) + ((tf.tv_usec - t0.tv_usec) / 1000.0);
+  printf("Kernel loading : %.3f ms\n", t);
+
+  a3data_t *m1 = artico3_alloc(ITERATIONS * N * sizeof (TYPE), "gemm", "m1", A3_P_I);
+  a3data_t *m2 = artico3_alloc(ITERATIONS * N * sizeof (TYPE), "gemm", "m2", A3_P_I);
+  a3data_t *prod = artico3_alloc(ITERATIONS * N * sizeof (TYPE), "gemm", "prod", A3_P_O);
+
+  for (int i = 0; i < ITERATIONS; i++) {
+    for (int j = 0; j < N; j++) {
+      m1[i*N + j] = ftoa3(args->m1[j]);
+      m2[i*N + j] = ftoa3(args->m2[j]);
+    }
+  }
+
+  gettimeofday(&t0, NULL);
+  artico3_kernel_execute("gemm", ITERATIONS, 1);
+  artico3_kernel_wait("gemm");
+  gettimeofday(&tf, NULL);
+  t = ((tf.tv_sec - t0.tv_sec) * 1000.0) + ((tf.tv_usec - t0.tv_usec) / 1000.0);
+  printf("Kernel execution : %.3f ms (average : %.3f ms)\n", t, t/ITERATIONS);
+
+  for (int j = 0; j < N; j++) {
+      args->prod[j] = a3tof(prod[j]);
+  }
+
+  artico3_free("gemm", "m1");
+  artico3_free("gemm", "m2");
+  artico3_free("gemm", "prod");
+
+  artico3_kernel_release("gemm");
+
+  artico3_exit();
+}
+
+/* Input format:
+%% Section 1
+TYPE[N]: matrix 1
+%% Section 2
+TYPE[N]: matrix 2
+*/
+
+void input_to_data(int fd, void *vdata) {
+  struct bench_args_t *data = (struct bench_args_t *)vdata;
+  char *p, *s;
+  // Zero-out everything.
+  memset(vdata,0,sizeof(struct bench_args_t));
+  // Load input string
+  p = readfile(fd);
+
+  s = find_section_start(p,1);
+  STAC(parse_,TYPE,_array)(s, data->m1, N);
+
+  s = find_section_start(p,2);
+  STAC(parse_,TYPE,_array)(s, data->m2, N);
+  free(p);
+}
+
+void data_to_input(int fd, void *vdata) {
+  struct bench_args_t *data = (struct bench_args_t *)vdata;
+
+  write_section_header(fd);
+  STAC(write_,TYPE,_array)(fd, data->m1, N);
+
+  write_section_header(fd);
+  STAC(write_,TYPE,_array)(fd, data->m2, N);
+}
+
+/* Output format:
+%% Section 1
+TYPE[N]: output matrix
+*/
+
+void output_to_data(int fd, void *vdata) {
+  struct bench_args_t *data = (struct bench_args_t *)vdata;
+  char *p, *s;
+  // Load input string
+  p = readfile(fd);
+
+  s = find_section_start(p,1);
+  STAC(parse_,TYPE,_array)(s, data->prod, N);
+  free(p);
+}
+
+void data_to_output(int fd, void *vdata) {
+  struct bench_args_t *data = (struct bench_args_t *)vdata;
+
+  write_section_header(fd);
+  STAC(write_,TYPE,_array)(fd, data->prod, N);
+}
+
+int check_data( void *vdata, void *vref ) {
+  struct bench_args_t *data = (struct bench_args_t *)vdata;
+  struct bench_args_t *ref = (struct bench_args_t *)vref;
+  int has_errors = 0;
+  int r,c;
+  TYPE diff;
+
+  for( r=0; r<row_size; r++ ) {
+    for( c=0; c<col_size; c++ ) {
+      diff = data->prod[r*row_size + c] - ref->prod[r*row_size+c];
+      has_errors |= (diff<-EPSILON) || (EPSILON<diff);
+    }
+  }
+
+  // Return true if it's correct.
+  return !has_errors;
+}
