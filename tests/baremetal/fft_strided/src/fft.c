1786d0fecd035643f4b603d4369f6de77734aa7c
Alfonso Rodriguez <alfonso.rodriguezm@upm.es>
2020-02-06 13:58:30 +0100

    Removed array equalization in FFT/STRIDED (SDSoC)

--- tests/baremetal/fft_strided/src/fft.c
+++ tests/baremetal/fft_strided/src/fft.c
@@ -1,11 +1,11 @@
 #include "fft.h"
 
-void fft(float real[FFT_SIZE], float img[FFT_SIZE], float real_twid[FFT_SIZE], float img_twid[FFT_SIZE]){
+void fft(float real[FFT_SIZE], float img[FFT_SIZE], float real_twid[FFT_SIZE/2], float img_twid[FFT_SIZE/2]){
 #pragma HLS interface s_axilite port=return
 #pragma HLS INTERFACE m_axi depth=1024 port=real offset=slave
 #pragma HLS INTERFACE m_axi depth=1024 port=img offset=slave
-#pragma HLS INTERFACE m_axi depth=1024 port=real_twid offset=slave
-#pragma HLS INTERFACE m_axi depth=1024 port=img_twid offset=slave
+#pragma HLS INTERFACE m_axi depth=512 port=real_twid offset=slave
+#pragma HLS INTERFACE m_axi depth=512 port=img_twid offset=slave
     int even, odd, span, log, rootindex;
     float temp;
     log = 0;
--- tests/baremetal/fft_strided/src/fft.h
+++ tests/baremetal/fft_strided/src/fft.h
@@ -6,7 +6,7 @@
 #define twoPI 6.28318530717959
 
 //void fft(double real[FFT_SIZE], double img[FFT_SIZE], double real_twid[FFT_SIZE/2], double img_twid[FFT_SIZE/2]);
-void fft(float real[FFT_SIZE], float img[FFT_SIZE], float real_twid[FFT_SIZE], float img_twid[FFT_SIZE]);
+void fft(float real[FFT_SIZE], float img[FFT_SIZE], float real_twid[FFT_SIZE/2], float img_twid[FFT_SIZE/2]);
 
 
 ////////////////////////////////////////////////////////////////////////////////
