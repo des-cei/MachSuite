81fd45dda949f98d93dd07d405283e4538bafbd2
Alfonso Rodriguez <alfonso.rodriguezm@upm.es>
2020-02-06 17:21:02 +0100

    Added SORT/MERGE (SDSoC)

---
+++ tests/baremetal/sort_merge/src/local_support.c
@@ -0,0 +1,95 @@
+#include "sort.h"
+#include <string.h>
+#include <ff.h>      // FIL, f_xxx()
+#include <sds_lib.h> // sds_clock_counter()
+
+int INPUT_SIZE = sizeof(struct bench_args_t);
+float t;
+
+void run_benchmark( void *vargs ) {
+  struct bench_args_t *args = (struct bench_args_t *)vargs;
+  //~ ms_mergesort( args->a );
+
+  TYPE a[SIZE];
+  unsigned long long t0, tf;
+
+  memcpy(a, args->a, sizeof a);
+
+  t0 = 2 * sds_clock_counter(); // AR# 67763
+  ms_mergesort( a );
+  tf = 2 * sds_clock_counter(); // AR# 67763
+  t += (float)(tf-t0)/(sds_clock_frequency() / 1000);
+
+  memcpy(args->a, a, sizeof a);
+}
+
+/* Input format:
+%% Section 1
+TYPE[SIZE]: the array
+*/
+
+void input_to_data(FIL fd, void *vdata) { // void input_to_data(int fd, void *vdata) {
+  struct bench_args_t *data = (struct bench_args_t *)vdata;
+  char *p, *s;
+  // Zero-out everything.
+  memset(vdata,0,sizeof(struct bench_args_t));
+  // Load input string
+  p = readfile(fd);
+
+  s = find_section_start(p,1);
+  STAC(parse_,TYPE,_array)(s, data->a, SIZE);
+  free(p);
+}
+
+void data_to_input(FIL fd, void *vdata) { // void data_to_input(int fd, void *vdata) {
+  struct bench_args_t *data = (struct bench_args_t *)vdata;
+
+  write_section_header(fd);
+  STAC(write_,TYPE,_array)(fd, data->a, SIZE);
+}
+
+/* Output format:
+%% Section 1
+TYPE[SIZE]: the array
+*/
+
+void output_to_data(FIL fd, void *vdata) { // void output_to_data(int fd, void *vdata) {
+  struct bench_args_t *data = (struct bench_args_t *)vdata;
+  char *p, *s;
+  // Zero-out everything.
+  memset(vdata,0,sizeof(struct bench_args_t));
+  // Load input string
+  p = readfile(fd);
+
+  s = find_section_start(p,1);
+  STAC(parse_,TYPE,_array)(s, data->a, SIZE);
+  free(p);
+}
+
+void data_to_output(FIL fd, void *vdata) { // void data_to_output(int fd, void *vdata) {
+  struct bench_args_t *data = (struct bench_args_t *)vdata;
+
+  write_section_header(fd);
+  STAC(write_,TYPE,_array)(fd, data->a, SIZE);
+}
+
+int check_data( void *vdata, void *vref ) {
+  struct bench_args_t *data = (struct bench_args_t *)vdata;
+  struct bench_args_t *ref = (struct bench_args_t *)vref;
+  int has_errors = 0;
+  int i;
+  TYPE data_sum, ref_sum;
+
+  // Check sortedness and sum
+  data_sum = data->a[0];
+  ref_sum = ref->a[0];
+  for( i=1; i<SIZE; i++ ) {
+    has_errors |= data->a[i-1] > data->a[i];
+    data_sum += data->a[i];
+    ref_sum += ref->a[i];
+  }
+  has_errors |= (data_sum!=ref_sum);
+
+  // Return true if it's correct.
+  return !has_errors;
+}
