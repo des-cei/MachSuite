72c1004ca55fdaa9a56fa6fc5138fa92f2e08d97
Alfonso Rodriguez <alfonso.rodriguezm@upm.es>
2020-02-06 17:04:36 +0100

    Added BFS/QUEUE (SDSoC)

---
+++ tests/baremetal/bfs_queue/src/harness.c
@@ -0,0 +1,93 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <assert.h>
+#include <ff.h>      // FIL, f_xxx()
+
+#define WRITE_OUTPUT
+#define CHECK_OUTPUT
+
+#include "support.h"
+
+#define ITERATIONS (1024)
+extern float t;
+
+int main(int argc, char **argv)
+{
+  // Parse command line.
+  char *in_file;
+  #ifdef CHECK_OUTPUT
+  char *check_file;
+  #endif
+  assert( argc<4 && "Usage: ./benchmark <input_file> <check_file>" );
+  in_file = "bfs/queue/input.dat"; // in_file = "input.data";
+  #ifdef CHECK_OUTPUT
+  check_file = "bfs/queue/check.dat"; // check_file = "check.data";
+  #endif
+  if( argc>1 )
+    in_file = argv[1];
+  #ifdef CHECK_OUTPUT
+  if( argc>2 )
+    check_file = argv[2];
+  #endif
+
+  // Mount FatFs
+  FATFS fs;
+  FRESULT fres;
+  fres = f_mount(&fs, "", 0);
+  assert( fres==0 && "Couldn't mount filesystem" );
+
+  // Load input data
+  FIL in_fd; // int in_fd;
+  char *data;
+  data = malloc(INPUT_SIZE);
+  assert( data!=NULL && "Out of memory" );
+  fres = f_open(&in_fd, in_file, FA_READ); // in_fd = open( in_file, O_RDONLY );
+  assert( fres==FR_OK && "Couldn't open input data file" ); // assert( in_fd>0 && "Couldn't open input data file" );
+  input_to_data(in_fd, data);
+  f_close(&in_fd);
+
+  // Unpack and call
+  t = 0;
+  for (int i = 0; i < ITERATIONS; i++) run_benchmark( data );
+  printf("<BFS/QUEUE> Elapsed time : %.3f ms (average : %.3f ms)\n", t, t/ITERATIONS);
+
+  #ifdef WRITE_OUTPUT
+  FIL out_fd; // int out_fd;
+  fres = f_open(&out_fd, "bfs/queue/output.dat", FA_WRITE | FA_CREATE_ALWAYS); // out_fd = open("output.data", O_WRONLY|O_CREAT|O_TRUNC, S_IRUSR|S_IWUSR|S_IRGRP|S_IWGRP|S_IROTH|S_IWOTH);
+  assert( fres==FR_OK && "Couldn't open output data file" ); // assert( out_fd>0 && "Couldn't open output data file" );
+  data_to_output(out_fd, data);
+  f_close(&out_fd); // close(out_fd);
+  #endif
+
+  // Load check data
+  #ifdef CHECK_OUTPUT
+  FIL check_fd; // int check_fd;
+  char *ref;
+  ref = malloc(INPUT_SIZE);
+  assert( ref!=NULL && "Out of memory" );
+  fres = f_open(&check_fd, check_file, FA_READ); // check_fd = open( check_file, O_RDONLY );
+  assert( fres==FR_OK && "Couldn't open check data file" ); // assert( check_fd>0 && "Couldn't open check data file" );
+  output_to_data(check_fd, ref);
+  f_close(&check_fd);
+  #endif
+
+  // Validate benchmark results
+  #ifdef CHECK_OUTPUT
+  if( !check_data(data, ref) ) {
+    fprintf(stderr, "Benchmark results are incorrect\n");
+    return -1;
+  }
+  free(ref);
+  #endif
+  free(data);
+
+  // Unmount FatFs
+  f_mount(NULL, "", 0);
+
+  printf("Success.\n");
+  return 0;
+}
---
+++ tests/baremetal/bfs_queue/src/local_support.c
@@ -0,0 +1,154 @@
+#include "bfs.h"
+#include <string.h>
+#include <unistd.h>
+#include <ff.h>      // FIL, f_xxx()
+#include <sds_lib.h> // sds_clock_counter()
+
+int INPUT_SIZE = sizeof(struct bench_args_t);
+float t;
+
+void run_benchmark( void *vargs ) {
+  struct bench_args_t *args = (struct bench_args_t *)vargs;
+  //~ bfs(args->nodes, args->edges, args->starting_node, args->level, args->level_counts);
+
+  edge_index_t nodes_begin[N_EDGES], nodes_end[N_EDGES], level_counts[N_EDGES];
+  node_index_t edges[N_EDGES];
+  node_index_t starting_node[N_EDGES];
+  uint32_t level[N_EDGES];
+  static uint32_t aux1[N_EDGES];
+  static edge_index_t aux2[N_EDGES];
+  static char flag = 0;
+  unsigned long long t0, tf;
+
+  for (int j = 0; j < N_NODES; j++) {
+    nodes_begin[j] = args->nodes[j].edge_begin;
+    nodes_end[j] = args->nodes[j].edge_end;
+  }
+  if (flag == 0) {
+    for (int j = 0; j < N_NODES; j++) aux1[j] = args->level[j];
+    memcpy(aux2, args->level_counts, sizeof aux2);
+  }
+  flag = 1;
+  memcpy(level, aux1, sizeof level);
+  memset(level_counts, 0, sizeof level_counts) ;//memcpy(level_counts, aux2, sizeof level_counts);
+  for (int j = 0; j < N_EDGES; j++) {
+    edges[j] = args->edges[j].dst;
+  }
+  starting_node[0] = args->starting_node;
+
+  t0 = 2 * sds_clock_counter(); // AR# 67763
+  bfs(nodes_begin, nodes_end, edges, starting_node, level, level_counts);
+  tf = 2 * sds_clock_counter(); // AR# 67763
+  t += (float)(tf-t0)/(sds_clock_frequency() / 1000);
+
+  for (int j = 0; j < N_NODES; j++) {
+    args->level[j] = level[j];
+  }
+  for (int j = 0; j < N_LEVELS; j++) {
+    args->level_counts[j] = level_counts[j];
+  }
+}
+
+/* Input format:
+%% Section 1
+uint32_t[1]: starting node
+%% Section 2
+uint32_t[N_NODES*2]: node structures (start and end indices of edge lists)
+%% Section 3
+uint32_t[N_EDGES]: edges structures (just destination node id)
+*/
+
+void input_to_data(FIL fd, void *vdata) { // void input_to_data(int fd, void *vdata) {
+  struct bench_args_t *data = (struct bench_args_t *)vdata;
+  char *p, *s;
+  uint32_t *nodes;
+  int64_t i;
+
+  // Zero-out everything.
+  memset(vdata,0,sizeof(struct bench_args_t));
+  // Max-ify levels
+  for(i=0; i<N_NODES; i++) {
+    data->level[i]=MAX_LEVEL;
+  }
+  // Load input string
+  p = readfile(fd);
+  // Section 1: starting node
+  s = find_section_start(p,1);
+  parse_uint32_t_array(s, &data->starting_node, 1);
+
+  // Section 2: node structures
+  s = find_section_start(p,2);
+  nodes = (uint32_t *)malloc(N_NODES*2*sizeof(uint32_t));
+  parse_uint32_t_array(s, nodes, N_NODES*2);
+  for(i=0; i<N_NODES; i++) {
+    data->nodes[i].edge_begin = nodes[2*i];
+    data->nodes[i].edge_end = nodes[2*i+1];
+  }
+  free(nodes);
+  // Section 3: edge structures
+  s = find_section_start(p,3);
+  parse_uint32_t_array(s, (uint32_t *)(data->edges), N_EDGES);
+  free(p);
+}
+
+void data_to_input(FIL fd, void *vdata) { // void data_to_input(int fd, void *vdata) {
+  uint32_t *nodes;
+  int64_t i;
+
+  struct bench_args_t *data = (struct bench_args_t *)vdata;
+  // Section 1: starting node
+  write_section_header(fd);
+  write_uint32_t_array(fd, &data->starting_node, 1);
+  // Section 2: node structures
+  write_section_header(fd);
+  nodes = (uint32_t *)malloc(N_NODES*2*sizeof(uint32_t));
+  for(i=0; i<N_NODES; i++) {
+    nodes[2*i]  = data->nodes[i].edge_begin;
+    nodes[2*i+1]= data->nodes[i].edge_end;
+  }
+  write_uint32_t_array(fd, nodes, N_NODES*2);
+  free(nodes);
+  // Section 3: edge structures
+  write_section_header(fd);
+  write_uint32_t_array(fd, (uint32_t *)(&data->edges), N_EDGES);
+}
+
+/* Output format:
+%% Section 1
+uint32_t[N_LEVELS]: horizon counts
+*/
+
+void output_to_data(FIL fd, void *vdata) { // void output_to_data(int fd, void *vdata) {
+  struct bench_args_t *data = (struct bench_args_t *)vdata;
+  char *p, *s;
+  // Zero-out everything.
+  memset(vdata,0,sizeof(struct bench_args_t));
+  // Load input string
+  p = readfile(fd);
+  // Section 1: horizon counts
+  s = find_section_start(p,1);
+  parse_uint32_t_array(s, data->level_counts, N_LEVELS);
+  free(p);
+}
+
+void data_to_output(FIL fd, void *vdata) { // void data_to_output(int fd, void *vdata) {
+  struct bench_args_t *data = (struct bench_args_t *)vdata;
+  // Section 1
+  write_section_header(fd);
+  write_uint32_t_array(fd, data->level_counts, N_LEVELS);
+}
+
+int check_data( void *vdata, void *vref ) {
+  struct bench_args_t *data = (struct bench_args_t *)vdata;
+  struct bench_args_t *ref = (struct bench_args_t *)vref;
+  int has_errors = 0;
+  int i;
+
+  // Check that the horizons have the same number of nodes
+  for(i=0; i<N_LEVELS; i++) {
+    has_errors |= (data->level_counts[i]!=ref->level_counts[i]);
+  }
+
+  // Return true if it's correct.
+  return !has_errors;
+}
