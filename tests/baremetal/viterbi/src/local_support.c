be22d5c6fdaa35c123c19a7ded8852de795c906f
Alfonso Rodriguez <alfonso.rodriguezm@upm.es>
2020-02-07 10:10:08 +0100

    Added VITERBI/VITERBI (SDSoC)

---
+++ tests/baremetal/viterbi/src/local_support.c
@@ -0,0 +1,120 @@
+#include "viterbi.h"
+#include <string.h>
+#include <ff.h>      // FIL, f_xxx()
+#include <sds_lib.h> // sds_clock_counter()
+
+int INPUT_SIZE = sizeof(struct bench_args_t);
+float t;
+
+void run_benchmark( void *vargs ) {
+  struct bench_args_t *args = (struct bench_args_t *)vargs;
+  //~ viterbi( args->obs, args->init, args->transition, args->emission, args->path );
+
+  tok_t obs[N_STATES*N_STATES];
+  prob_t init[N_STATES*N_STATES];
+  prob_t transition[N_STATES*N_STATES];
+  prob_t emission[N_STATES*N_TOKENS];
+  state_t path[N_STATES*N_STATES];
+  unsigned long long t0, tf;
+
+  memcpy(obs, args->obs, sizeof args->obs);
+  memcpy(init, args->init, sizeof args->init);
+  memcpy(transition, args->transition, sizeof args->transition);
+  memcpy(emission, args->emission, sizeof args->emission);
+  memset(path, 0, sizeof path);
+
+  t0 = 2 * sds_clock_counter(); // AR# 67763
+  viterbi( obs, init, transition, emission, path );
+  tf = 2 * sds_clock_counter(); // AR# 67763
+  t += (float)(tf-t0)/(sds_clock_frequency() / 1000);
+
+  memcpy(args->path, path, sizeof args->path);
+}
+
+/* Input format:
+%% Section 1
+tok_t[N_OBS]: observation vector
+%% Section 2
+prob_t[N_STATES]: initial state probabilities
+%% Section 3
+prob_t[N_STATES*N_STATES]: transition matrix
+%% Section 4
+prob_t[N_STATES*N_TOKENS]: emission matrix
+*/
+
+void input_to_data(FIL fd, void *vdata) { // void input_to_data(int fd, void *vdata) {
+  struct bench_args_t *data = (struct bench_args_t *)vdata;
+  char *p, *s;
+  // Zero-out everything.
+  memset(vdata,0,sizeof(struct bench_args_t));
+  // Load input string
+  p = readfile(fd);
+
+  s = find_section_start(p,1);
+  parse_uint8_t_array(s, data->obs, N_OBS);
+
+  s = find_section_start(p,2);
+  STAC(parse_,TYPE,_array)(s, data->init, N_STATES);
+
+  s = find_section_start(p,3);
+  STAC(parse_,TYPE,_array)(s, data->transition, N_STATES*N_STATES);
+
+  s = find_section_start(p,4);
+  STAC(parse_,TYPE,_array)(s, data->emission, N_STATES*N_TOKENS);
+  free(p);
+}
+
+void data_to_input(FIL fd, void *vdata) { // void data_to_input(int fd, void *vdata) {
+  struct bench_args_t *data = (struct bench_args_t *)vdata;
+
+  write_section_header(fd);
+  write_uint8_t_array(fd, data->obs, N_OBS);
+
+  write_section_header(fd);
+  STAC(write_,TYPE,_array)(fd, data->init, N_STATES);
+
+  write_section_header(fd);
+  STAC(write_,TYPE,_array)(fd, data->transition, N_STATES*N_STATES);
+
+  write_section_header(fd);
+  STAC(write_,TYPE,_array)(fd, data->emission, N_STATES*N_TOKENS);
+}
+
+/* Output format:
+%% Section 1
+uint8_t[N_OBS]: most likely state chain
+*/
+
+void output_to_data(FIL fd, void *vdata) { // void output_to_data(int fd, void *vdata) {
+  struct bench_args_t *data = (struct bench_args_t *)vdata;
+  char *p, *s;
+  // Zero-out everything.
+  memset(vdata,0,sizeof(struct bench_args_t));
+  // Load input string
+  p = readfile(fd);
+
+  s = find_section_start(p,1);
+  parse_uint8_t_array(s, data->path, N_OBS);
+  free(p);
+}
+
+void data_to_output(FIL fd, void *vdata) { // void data_to_output(int fd, void *vdata) {
+  struct bench_args_t *data = (struct bench_args_t *)vdata;
+
+  write_section_header(fd);
+  write_uint8_t_array(fd, data->path, N_OBS);
+}
+
+int check_data( void *vdata, void *vref ) {
+  struct bench_args_t *data = (struct bench_args_t *)vdata;
+  struct bench_args_t *ref = (struct bench_args_t *)vref;
+  int has_errors = 0;
+  int i;
+
+  for(i=0; i<N_OBS; i++) {
+    has_errors |= (data->path[i]!=ref->path[i]);
+  }
+
+  // Return true if it's correct.
+  return !has_errors;
+}
